68ebcf062dd7788fbdafb68643516954d05761a4
rochelle-rossman <rochelle.rossman@gmail.com>
2023-05-22 10:29:27 -0500

    wp update

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
-function university_files() {
+function university_files()
+{
   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
   wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
@@ -9,7 +10,8 @@ function university_files() {
 }
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features() {
+function university_features()
+{
   // register_nav_menu('headerMenuLocation', 'Header Menu Location');
   // register_nav_menu('footerLocationOne', 'Footer Location One');
   // register_nav_menu('footerLocationTwo', 'Footer Location Two ');
@@ -17,5 +19,3 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
-
-?>
